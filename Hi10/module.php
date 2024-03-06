d7f059991c56855449c6034dbd910ddc9ea6e030
madrosDK <danielkoglek@gmx.at>
2024-03-06 16:07:24 +0100

    Update module.php

--- Hi10/module.php
+++ Hi10/module.php
@@ -26,19 +26,19 @@ class HI10 extends AlphaESS
 
     public static $Variables = [
         ['Voltage', VARIABLETYPE_FLOAT, 'Volt.230', 0x0000, 4, 2, true],
-        /*['Current', VARIABLETYPE_FLOAT, 'Ampere', 0x0006, 4, 2, true],
-        ['Active power', VARIABLETYPE_FLOAT, 'Watt.14490', 0x000C, 4, 2, true],
-        ['Apparent power', VARIABLETYPE_FLOAT, 'VA', 0x0012, 4, 2, true],
-        ['Reactive power', VARIABLETYPE_FLOAT, 'VaR', 0x0018, 4, 2, true],
-        ['Power factor', VARIABLETYPE_FLOAT, '', 0x001E, 4, 2, true],
-        ['Phase angle', VARIABLETYPE_FLOAT, 'PhaseAngle', 0x0024, 4, 2, true],
-        ['Frequency', VARIABLETYPE_FLOAT, 'Hertz.50', 0x0046, 4, 2, true],
-        ['Total import energy', VARIABLETYPE_FLOAT, 'Electricity', 0x0048, 4, 2, true],
-        ['Total export energy', VARIABLETYPE_FLOAT, 'Electricity', 0x004A, 4, 2, true],
-        ['Total import reactive energy', VARIABLETYPE_FLOAT, 'kVArh', 0x004C, 4, 2, true],
-        ['Total export reactive energy', VARIABLETYPE_FLOAT, 'kVArh', 0x004E, 4, 2, true],
-        ['Total active energy', VARIABLETYPE_FLOAT, 'Electricity', 0x0156, 4, 2, true],
-        ['Total active energy', VARIABLETYPE_FLOAT, 'Electricity', 0x0156, 4, 2, true], */
+    //    ['Current', VARIABLETYPE_FLOAT, 'Ampere', 0x0006, 4, 2, true],
+    //    ['Active power', VARIABLETYPE_FLOAT, 'Watt.14490', 0x000C, 4, 2, true],
+    //    ['Apparent power', VARIABLETYPE_FLOAT, 'VA', 0x0012, 4, 2, true],
+    //    ['Reactive power', VARIABLETYPE_FLOAT, 'VaR', 0x0018, 4, 2, true],
+    //    ['Power factor', VARIABLETYPE_FLOAT, '', 0x001E, 4, 2, true],
+    //    ['Phase angle', VARIABLETYPE_FLOAT, 'PhaseAngle', 0x0024, 4, 2, true],
+    //    ['Frequency', VARIABLETYPE_FLOAT, 'Hertz.50', 0x0046, 4, 2, true],
+    //    ['Total import energy', VARIABLETYPE_FLOAT, 'Electricity', 0x0048, 4, 2, true],
+    //    ['Total export energy', VARIABLETYPE_FLOAT, 'Electricity', 0x004A, 4, 2, true],
+    //    ['Total import reactive energy', VARIABLETYPE_FLOAT, 'kVArh', 0x004C, 4, 2, true],
+    //    ['Total export reactive energy', VARIABLETYPE_FLOAT, 'kVArh', 0x004E, 4, 2, true],
+    //    ['Total active energy', VARIABLETYPE_FLOAT, 'Electricity', 0x0156, 4, 2, true],
+    //    ['Total active energy', VARIABLETYPE_FLOAT, 'Electricity', 0x0156, 4, 2, true], 
         ['PV1 power', VARIABLETYPE_INT, 'Watt.14490', 0x041F, 3, 2, true]
     ];
 }
