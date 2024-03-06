3e41f6b04659ed7330fb45bd39b49effcd87b225
madrosDK <danielkoglek@gmx.at>
2024-03-06 14:56:19 +0100

    -

--- Hi10/module.php
+++ Hi10/module.php
@@ -14,15 +14,15 @@ declare(strict_types=1);
  * @version       3.51
  *
  */
-require_once __DIR__ . '/../libs/BGETechModule.php';  // diverse Klassen
+require_once __DIR__ . '/../libs/AlphaESSModule.php';  // diverse Klassen
 
 /**
  * SDM220 ist die Klasse für die SDM220 ModBus Energie-Zähler der Firma B+G E-Tech
  * Erweitert BGETech.
  */
-class SDM220 extends BGETech
+class HI10 extends AlphaESS
 {
-    const PREFIX = 'SDM220';
+    const PREFIX = 'AlphaESS';
 
     public static $Variables = [
         ['Voltage', VARIABLETYPE_FLOAT, 'Volt.230', 0x0000, 4, 2, true],
