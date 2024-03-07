d14c367d308a45ef22c9bdc11e088e02535b48c9
madrosDK <danielkoglek@gmx.at>
2024-03-07 08:00:05 +0100

    Update AlphaESSModule.php

--- libs/AlphaESSModule.php
+++ libs/AlphaESSModule.php
@@ -16,3 +16,46 @@ declare(strict_types=1);
  */
 require_once __DIR__ . '/SemaphoreHelper.php';  // diverse Klassen
 eval('declare(strict_types=1);namespace AlphaESS {?>' . file_get_contents(__DIR__ . '/helper/VariableProfileHelper.php') . '}');
+
+/**
+ * BGETech ist die Basisklasse für alle Energie-Zähler der Firma B+G E-Tech
+ * Erweitert ipsmodule.
+ * @property array $Variables
+ * @method void RegisterProfileInteger(string $Name, string $Icon, string $Prefix, string $Suffix, int $MinValue, int $MaxValue, float $StepSize)
+ * @method void RegisterProfileFloat(string $Name, string $Icon, string $Prefix, string $Suffix, float $MinValue, float $MaxValue, float $StepSize, int $Digits)
+ */
+
+
+class AlphaESS extends IPSModule
+{
+    use \AlphaESS\SemaphoreHelper;
+    use \AlphaESS\VariableProfileHelper;
+    const Swap = true;
+    const PREFIX = '';
+    /**
+     * Interne Funktion des SDK.
+     */
+    public function Create()
+    {
+        parent::Create();
+        $this->ConnectParent('{A5F663AB-C400-4FE5-B207-4D67CC030564}');
+        $this->RegisterPropertyInteger('Interval', 0);
+        $Variables = [];
+        foreach (static::$Variables as $Pos => $Variable) {
+            $Variables[] = [
+                'Ident'    => str_replace(' ', '', $Variable[0]),
+                'Name'     => $this->Translate($Variable[0]),
+                'VarType'  => $Variable[1],
+                'Profile'  => $Variable[2],
+                'Address'  => $Variable[3],
+                'Function' => $Variable[4],
+                'Quantity' => $Variable[5],
+                'Pos'      => $Pos + 1,
+                'Keep'     => $Variable[6]
+            ];
+        }
+        $this->RegisterPropertyString('Variables', json_encode($Variables));
+        $this->RegisterTimer('UpdateTimer', 0, static::'ModBus_RequestRead($_IPS["TARGET"]);');
+    }
+
+}
