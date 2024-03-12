452f89acef33799ed55dd4aeb6f3cf18b5c44168
madrosDK <danielkoglek@gmx.at>
2024-03-12 15:43:53 +0100

    Update AlphaESSModule.php

--- libs/AlphaESSModule.php
+++ libs/AlphaESSModule.php
@@ -50,7 +50,7 @@ class AlphaESS extends IPSModule
     {
         parent::ApplyChanges();
 
-        $NewRows = static::$value; //NEU
+        $NewRows = $this->GetDeviceTemplate( static::DeviceIdent ); //NEU
         $NewPos = 0;  //NEU
 
         $variables = json_decode ( $this->ReadPropertyString("Variables"), true);
