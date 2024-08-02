dd41a7c08c0a073bde00eeb5b907e1a4c5580125
madrosDK <danielkoglek@gmx.at>
2024-08-02 11:03:34 +0200

    Update AlphaESSModule.php

--- libs/AlphaESSModule.php
+++ libs/AlphaESSModule.php
@@ -64,7 +64,7 @@ class AlphaESS extends IPSModule
         }
 
         $variables = $this->GetDeviceTemplate( static::DeviceIdent );
-	    $this->RegisterPropertyString("Variables", json_encode ( $variables ) );
+	    $this->ReadPropertyString("Variables", json_encode ( $variables ) );
         /*
             foreach ($NewRows as $Index => $Row) {    //NEU ab hier
               //if ($value['Ident'] == str_replace(" ", "", $Row[0])) {
